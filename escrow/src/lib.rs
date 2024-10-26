359f4ceb7b51a3a1d84759a5213b84b52254da11
Dean 利迪恩 <dean@bitping.com>
2024-10-26 17:12:36 +0800

    entrypoint

--- escrow/src/lib.rs
+++ escrow/src/lib.rs
@@ -1,9 +1,34 @@
-pub const ID: [u8;32] = five8_const::decode_32_const("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
-pub const PDA_MARKER: &[u8; 21] = b"ProgramDerivedAddress";
+use make::make;
+use pinocchio::account_info::AccountInfo;
+use pinocchio::pubkey::Pubkey;
+use pinocchio::{entrypoint::ProgramResult, program_error::ProgramError};
+use pinocchio::entrypoint;
 
+mod instructions;
+use instructions::*;
+use refund::refund;
+use take::take;
 mod state;
 mod make;
 mod refund;
 mod take;
-
 mod pinocchio_spl;
+
+
+entrypoint!(process_instruction);
+
+pub const ID: [u8;32] = five8_const::decode_32_const("22222222222222222222222222222222222222222222");
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let (discriminator, data) = instruction_data.split_first().ok_or(ProgramError::InvalidInstructionData)?;
+
+    match EscrowInstruction::try_from(discriminator)? {
+        EscrowInstruction::Make => make(accounts, data),
+        EscrowInstruction::Take => take(accounts),
+        EscrowInstruction::Refund => refund(accounts),
+    }
+}
